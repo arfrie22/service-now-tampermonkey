cbaf04c25d61fb375c455122b7e1c53e30591106
arfrie22 <43021241+arfrie22@users.noreply.github.com>
2023-06-16 16:27:03 -0400

    Added more sn urls to new tab

--- service_now_new_tab_fixer.user.js
+++ service_now_new_tab_fixer.user.js
@@ -3,10 +3,14 @@
 // @namespace   Violentmonkey Scripts
 // @match       https://*.service-now.com/sc*.do
 // @match       https://*.service-now.com/kb*.do
-// @match       https://*.service-now.com/incident.do
+// @match       https://*.service-now.com/incident*.do
+// @match       https://*.service-now.com/reconcile*.do
+// @match       https://*.service-now.com/sysapproval*.do
+// @match       https://*.service-now.com/problem*.do
+// @match       https://*.service-now.com/upgrade*.do
 // @match       https://*.service-now.com/cmdb*.do
 // @grant       none
-// @version     1.1.1
+// @version     1.1.2
 // @author      Andrew Friedman
 // @description Fixes service now not showing the main bar if you open a tab in a new url
 // @updateURL   https://github.com/arfrie22/service-now-tampermonkey/raw/main/service_now_new_tab_fixer.user.js
